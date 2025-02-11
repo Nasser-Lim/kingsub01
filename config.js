ed0bc31432b83648fae77fe8b598b442582fb313
Nasser-Lim <156804499+Nasser-Lim@users.noreply.github.com>
2025-02-11 14:44:22 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 export const SHEET_ID = '13nIkOkiS0eVvjIy6GN8tSmMVoXIjyhxAL_jDHbNJI9k';
-export const TAB_NAME = '킹자막기v1.0';
+export const TAB_NAME = '킹자막기v1.0M';
 export const API_KEY = 'AIzaSyBloGeLveVT_ORsJDWIXWe0PqCWCsnuKOE';
